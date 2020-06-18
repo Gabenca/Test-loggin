f216b4b3770fc1f2ef6ef405840f0e685da61a00
Gabenca <52291309+Gabenca@users.noreply.github.com>
2020-06-18 19:45:07 +0700

    Add files via upload

--- monitoring.py
+++ monitoring.py
@@ -15,10 +15,22 @@ import random
 import time
 # Yaml reader:
 import yaml
+# Resolve dns:
+import dns.resolver
+# Selenium webdriver:
+from selenium import webdriver
+# Selenium webdriver Chrome:
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 ###################################  Class  ###################################
 ###############################################################################
 c = Counter('ErrorCount', 'Amount of mistakes')
+with open('fronts.yml') as f:
+    templates = yaml.safe_load(f)
+    for key, value in templates['fronts'].items():
+        key = Counter('ErrorCount_'+ key, 'Amount of mistakes for url' + key)
 
 class LoggingRequests:
 
@@ -28,58 +40,72 @@ class LoggingRequests:
     #--------------------------------------------------------------------------
     # Class instance constructor
     #--------------------------------------------------------------------------
-    def __init__(self,
-        url):
+    def __init__(self,key,value,logger):
 
-        self.Errors
+        self.Errors = 0
 
-        self.Summ
+        self.Summ  = 0
 
-        def _request(self, url):
-            logger = logging.getLogger("exampleApp")
-            logger.setLevel(logging.INFO)
-
-            # create the logging file handler
-            headfh = url[8:-1]
-            fh = logging.FileHandler(f"log-{headfh}.log")
-
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            fh.setFormatter(formatter)
-
-            # add handler to logger object
-            logger.addHandler(fh)
+    def request(self):
+        try:
+            result = dns.resolver.query(value[8:])
+        except dns.resolver.NXDOMAIN:
+            logger.info("ошибка домен не найден в списке DNS: ", value)
+        else:
             try:
-                rs = requests.get(url)
+                rs = requests.get(value)
             except requests.ConnectionError:
                 self.Errors+=1
                 c.inc()
+                key.inc()
                 self.Summ+=1
-                logger.info("ошибка подключения к url: ", url)
+                logger.info("ошибка подключения к url: ", value)
             except requests.Timeout:
                 self.Errors+=1
                 c.inc()
+                key.inc()
                 self.Summ+=1
-                logger.info("ошибка timeout, url:", url)
+                logger.info("ошибка timeout, url:", value)
             except requests.HTTPError as err:
                 code = rs.status_code
                 self.Errors+=1
                 c.inc()
+                key.inc()
                 self.Summ+=1
-                logger.info(f"ошибка url: {url}, code: {code}")
+                logger.info(f"ошибка url: {value}, code: {code}")
             except requests.RequestException:
                 self.Errors+=1
                 c.inc()
+                key.inc()
                 self.Summ+=1
-                logger.info("ошибка скачивания url: ", url)
+                logger.info("ошибка скачивания url: ", value)
             except requests.TooManyRedirects:
                 self.Errors+=1
                 c.inc()
+                key.inc()
                 self.Summ+=1
-                logger.info("ошибка слишком много обращений к url: ", url)
+                logger.info("ошибка слишком много обращений к url: ", value)
             else:
                 code = rs.status_code
                 self.Summ+=1
-                logger.info(f"Запрос к url: {url} завершился с кодом: {code}")
+                logger.info(f"Запрос к url: {value} завершился с кодом: {code}")
+                #  Open Chrom
+                driver = Chrome('C:\Chromiuuus\chromedriver.exe')
+                #  Go to url
+                driver.get(value)
+                driver.implicitly_wait(4)
+                search_input = driver.find_element_by_id('login' or 'loginInput')
+                if search_input:
+                #  Input text
+                    search_input.send_keys('Selenium')
+                    search_input.send_keys(Keys.RETURN)
+                else:
+                    self.Errors+=1
+                    c.inc()
+                    key.inc()
+                    self.Summ+=1
+                    logger.info("ошибка сайт не отображается в браузере: ", value)
+
 
 # Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
@@ -99,11 +125,26 @@ def f():
 ###############################################################################
 
 if __name__ == '__main__':
+    # add handler to logger object
+
+    logger = logging.getLogger("exampleApp")
+    logger.setLevel(logging.INFO)
+
+    # create the logging file handler
+    headfh = value[8:0]
+    fh = logging.FileHandler(f"log-CDEK.log")
+
+    logger.addHandler(fh)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
     # Initializations fronts.
     with open('fronts.yml') as f:
         templates = yaml.safe_load(f)
     for key, value in templates['fronts'].items():
-        print(value)
+        loging = LoggingRequests(key,value,logger)
+        loging.request()
+
 
     # Count only one type of exception
     with c.count_exceptions(ValueError):
